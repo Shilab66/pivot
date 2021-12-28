6c4eaeb1e5f9884f54b05630852818cac071cd63
Shilab66 <89350258+Shilab66@users.noreply.github.com>
2021-12-28 12:08:19 -0500

    Update popup.js

--- popup.js
+++ popup.js
@@ -1,1 +1,5 @@
-
+document.addEventListener('DOMContentLoaded', function() {
+    var canvasButton = document.getElementById('canvas');
+    var synergyButton = document.getElementById('synergy');
+    canvasButton.addEventListener('click', function() {}
+    synergyButton.addEventListener('click', function() {}
