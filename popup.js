c46241fd7ef01e09a291100e1d3ff49eafe1baf2
Shilab66 <89350258+Shilab66@users.noreply.github.com>
2021-12-28 14:33:38 -0500

    Update popup.js

--- popup.js
+++ popup.js
@@ -1,12 +1,15 @@
 document.addEventListener('DOMContentLoaded', function() {
     var canvasButton = document.getElementById('canvas');
     var synergyButton = document.getElementById('synergy');
+
     canvasButton.addEventListener('click', function(){
-        var newURL = "http://stackoverflow.com/";
+        var newURL = "https://mcpsmd.instructure.com/";
         chrome.tabs.create({ url: newURL });
-    });
+    }, false);
+
     synergyButton.addEventListener('click', function(){
-        var newURL = "http://stackoverflow.com/";
-        chrome.tabs.create({ url: newURL });
-    });
-}
+        var newURL2 = "https://md-mcps-psv.edupoint.com/PXP2_Login_Student.aspx?regenerateSessionId=True";
+        chrome.tabs.create({ url: newURL2 });
+    }, false);
+
+}, false);
