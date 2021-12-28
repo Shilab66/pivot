f4b94686e58c90b93086cacf372c4a80a8961d77
Shilab66 <89350258+Shilab66@users.noreply.github.com>
2021-12-28 14:18:24 -0500

    Update popup.js

--- popup.js
+++ popup.js
@@ -2,9 +2,11 @@ document.addEventListener('DOMContentLoaded', function() {
     var canvasButton = document.getElementById('canvas');
     var synergyButton = document.getElementById('synergy');
     canvasButton.addEventListener('click', function(){
-        
+        var newURL = "http://stackoverflow.com/";
+        chrome.tabs.create({ url: newURL });
     });
     synergyButton.addEventListener('click', function(){
-        
+        var newURL = "http://stackoverflow.com/";
+        chrome.tabs.create({ url: newURL });
     });
 }
