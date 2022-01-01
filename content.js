84a950d7cc0f239833afbf7f696cd20a4c76e077
Shilab66 <89350258+Shilab66@users.noreply.github.com>
2021-12-31 20:21:37 -0500

    Update content.js

--- content.js
+++ content.js
@@ -1,5 +1,9 @@
 var username = document.getElementById("ctl00_MainContent_username");
+var password = document.getElementById("ctl00_MainContent_username");
 
 username.addEventListener("click", () => {
-	alert('hello');
+	alert('username clicked');
+});
+password.addEventListener("click", () => {
+	alert('password clicked');
 });
