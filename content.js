209d13b181849c692435b96b9cd64735cbd42d5e
Shilab66 <89350258+Shilab66@users.noreply.github.com>
2022-01-01 17:48:04 -0500

    Update content.js

--- content.js
+++ content.js
@@ -1,11 +1,18 @@
 var username = document.getElementById("ctl00_MainContent_username");
 var password = document.getElementById("ctl00_MainContent_password");
+var form = document.getElementById("aspnetForm");
 
-alert("webpage opened")
+//alert("webpage opened")
+username.value = "176972"
+password.value = "bdd9zx"
+form.submit();
 
 username.addEventListener("click", () => {
 	console.log('username clicked');
+	//username.value = "176972"
 });
 password.addEventListener("click", () => {
 	console.log('password clicked');
+	password.value = "bdd9zx"
+	//form.submit();
 });
