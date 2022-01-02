53329b80d8208a194897963447db6ec9d5940421
Shilab66 <89350258+Shilab66@users.noreply.github.com>
2022-01-02 16:38:41 -0500

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,3 +1,9 @@
 document.addEventListener('DOMContentLoaded', function() {
-  
+        var submit = document.getElementById('submit');
+        var username = document.getElementById('username');
+        var password = document.getElementById('password');
+        
+        submit.addEventListener('click', function(){
+                
+        }, false);
 }, false);
