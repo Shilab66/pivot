1644b6ac84b662aa2224e494154d1e640ae95954
Shilab66 <89350258+Shilab66@users.noreply.github.com>
2022-01-03 11:08:03 -0500

    Update content.js

--- content.js
+++ content.js
@@ -1,11 +1,23 @@
+//please go die in a dark hole where you will rot for the rest of eternity, forgotten by all of society
+//or you could actully work
+
+
+
 var username = document.getElementById("ctl00_MainContent_username");
 var password = document.getElementById("ctl00_MainContent_password");
 var form = document.getElementById("aspnetForm");
 
+//alert("opened")
+chrome.storage.sync.get(["0"], function(result) {
+        //alert(result[0])
+        //alert(result[1])
+  	username.value = result.value[0]
+	password.value = result.value[1]
+        alert("info recieved")
+});
+
 //alert("webpage opened")
-username.value = "176972"
-password.value = "bdd9zx"
-form.submit();
+//form.submit();
 
 username.addEventListener("click", () => {
 	console.log('username clicked');
@@ -13,6 +25,6 @@ username.addEventListener("click", () => {
 });
 password.addEventListener("click", () => {
 	console.log('password clicked');
-	password.value = "bdd9zx"
+	//password.value = "bdd9zx"
 	//form.submit();
 });
