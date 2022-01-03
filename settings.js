227d5926412bff16c621035dc8d94859257725a3
Shilab66 <89350258+Shilab66@users.noreply.github.com>
2022-01-03 11:08:38 -0500

    Update settings.js

--- settings.js
+++ settings.js
@@ -4,8 +4,8 @@ document.addEventListener('DOMContentLoaded', function() {
         var password = document.getElementById('password');
         
         submit.addEventListener('click', function(){
-                chrome.storage.sync.set({0: [username,password]}, function() {
-  	                alert('Value is set to ' + value);
+                chrome.storage.sync.set({"0": [username.value, password.value]}, function() {
+  	                alert('Username and password are updated');
                  });
         }, false);
 }, false);
