a210d2d184773bbdac9946809dfe77ae1a9f74ab
Shilab66 <89350258+Shilab66@users.noreply.github.com>
2022-01-03 21:10:41 -0500

    Update settings.js

--- settings.js
+++ settings.js
@@ -4,8 +4,14 @@ document.addEventListener('DOMContentLoaded', function() {
         var password = document.getElementById('password');
         
         submit.addEventListener('click', function(){
-                chrome.storage.sync.set({"0": [username.value, password.value]}, function() {
-  	                alert('Username and password are updated');
+		const store = new Array(username.value, password.value)
+                chrome.storage.sync.set({"a": store}, function() {
+  	                //alert(username.value + password.value);
                  });
+
+		chrome.storage.sync.get(["a"], function(result) {
+			value = result["a"]
+        		alert(value[0]+ value[1])
+		});
         }, false);
 }, false);
