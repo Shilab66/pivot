0a7dc14b6ba67cac370172df86c0e6013b7709eb
Shilab66 <89350258+Shilab66@users.noreply.github.com>
2022-01-26 17:42:08 -0500

    Update serviceWorker.js

--- serviceWorker.js
+++ serviceWorker.js
@@ -1,12 +1,7 @@
-var doc = "https://md-mcps-psv.edupoint.com/PXP2_Login_Student.aspx?Logout=1&regenerateSessionId=True"
-
-var username = doc.getElementById("ctl00_MainContent_username");
-var password = doc.getElementById("ctl00_MainContent_password");
-var form = doc.getElementById("aspnetForm");
-
-chrome.storage.sync.get(["a"], function(result) {
-        value = result["a"]
-  	username.value = atob(value[0])
-	password.value = atob(value[1])
-	form.submit()
-});
+var request = new XMLHttpRequest();
+request.open("GET", "http://help.websiteos.com/websiteos/example_of_a_simple_html_page.htm");
+request.onload = function () {
+  var data = request.responseText;
+  const  = data.getElementByTagName('<p>')
+};
+request.send();
